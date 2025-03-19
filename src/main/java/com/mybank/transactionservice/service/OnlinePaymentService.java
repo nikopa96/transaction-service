275b5259b54029a8fea8e8025a204c71de22afb5
nikopa96 <jertyk@gmail.com>
2025-03-20 00:00:17 +0200

    sendAckToPaymentGateway refactoring

--- src/main/java/com/mybank/transactionservice/service/OnlinePaymentService.java
+++ src/main/java/com/mybank/transactionservice/service/OnlinePaymentService.java
@@ -5,13 +5,15 @@ import com.mybank.transactionservice.dto.PaymentGatewayAck;
 import com.mybank.transactionservice.exception.OnlinePaymentException;
 import com.mybank.transactionservice.exception.TransactionException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestClient;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OnlinePaymentService {
 
     private final TransactionService transactionService;
@@ -33,6 +35,7 @@ public class OnlinePaymentService {
                     .build();
 
             sendAckToPaymentGateway(failedTransaction, request.getCallbackURL());
+
             throw new OnlinePaymentException("Unable to make online payment",
                     FailedOnlinePaymentReason.valueOf(e.getReason().getValue()));
         }
@@ -44,18 +47,17 @@ public class OnlinePaymentService {
                 .success(transactionResponse.getSuccess())
                 .build();
 
-        ResponseEntity<Void> gatewayResponse = restClient.post()
-                .uri(callbackUrl)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(ack)
-                .retrieve()
-                .toBodilessEntity();
+        try {
+            restClient.post()
+                    .uri(callbackUrl)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(ack)
+                    .retrieve()
+                    .toBodilessEntity();
+        } catch (HttpStatusCodeException e) {
+            log.error("Unable to send an ack to gateway payment service. Status code: {}", e.getStatusCode().value());
 
-        if (gatewayResponse.getStatusCode().is2xxSuccessful()) {
-            // log ok
-        } else {
-            // log fail
-            throw new OnlinePaymentException("Unable to find a bank account balance by UUID.",
+            throw new OnlinePaymentException("Unable to send an ack to gateway payment service",
                     FailedOnlinePaymentReason.NO_RESPONSE_FROM_PAYMENT_GATEWAY_SERVER);
         }
     }
